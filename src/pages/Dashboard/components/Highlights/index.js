61032057bc05d300eff9b20f60088de4e8ea4306
Guilherme Di Paula <guilepaul@gmail.com>
2020-11-17 20:30:12 -0300

    highlights componet

--- src/pages/Dashboard/components/Highlights/index.js
+++ src/pages/Dashboard/components/Highlights/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
-import icon from '../../../../assets/profileIcon.png'
+import money from '../../../../assets/moneyIcon.png'
+import weddings from '../../../../assets/weddingsIcon.png'
+import newUsers from '../../../../assets/newUsersIcon.png'
+import newAccess from '../../../../assets/newAccessIcon.png'
 
 import * as S from './styles'
 
@@ -8,27 +11,27 @@ const Highlights = () => {
     return(
         <S.Conatiner>
             <S.Indicator>
-                <img src={icon} alt="tests" />
-                <S.ValueIndicator>856</S.ValueIndicator>
-                <S.LabelIndicator>Novos Cadastros</S.LabelIndicator>
+                <img src={newAccess} alt="acessos" />
+                <S.ValueIndicator>1476</S.ValueIndicator>
+                <S.LabelIndicator>Novos Acessos</S.LabelIndicator>
             </S.Indicator>
             <S.Separator />
             <S.Indicator>
-                <img src={icon} alt="tests" />
+                <img src={newUsers} alt="users" />
                 <S.ValueIndicator>856</S.ValueIndicator>
-                <S.LabelIndicator>Novos Cadastros</S.LabelIndicator>
+                <S.LabelIndicator>Novos Usuários</S.LabelIndicator>
             </S.Indicator>
             <S.Separator />
             <S.Indicator>
-                <img src={icon} alt="tests" />
-                <S.ValueIndicator>856</S.ValueIndicator>
-                <S.LabelIndicator>Novos Cadastros</S.LabelIndicator>
+                <img src={weddings} alt="weddings" />
+                <S.ValueIndicator>365</S.ValueIndicator>
+                <S.LabelIndicator>Casamentos</S.LabelIndicator>
             </S.Indicator>
             <S.Separator />
             <S.Indicator>
-                <img src={icon} alt="tests" />
-                <S.ValueIndicator>856</S.ValueIndicator>
-                <S.LabelIndicator>Novos Cadastros</S.LabelIndicator>
+                <img src={money} alt="faturamento" />
+                <S.ValueIndicator>R$ 3.5 M</S.ValueIndicator>
+                <S.LabelIndicator>Faturamento</S.LabelIndicator>
             </S.Indicator>
         </S.Conatiner>
     )
