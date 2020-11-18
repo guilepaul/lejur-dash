6b2c8010bdd96f9497976a958215aefb2a2a913a
Renan Mendes <renan.mendes@primepass.com.br>
2020-11-18 19:15:11 -0300

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import * as S from './styles'
+import insta from "../../assets/insta.png";
+import face from "../../assets/face.png";
+import linkedin from "../../assets/linkedin.png";
+
+import * as S from "./styles";
 
 const Footer = () => {
-    return (
-        <S.Container />
-    )
-}
+  return (
+    <S.Container>
+      {/* <S.MediaWrapper> */}
+      {/* <S.Text>
+        Encontre e contrate o fornecedor ideal para o seu casamento.
+      </S.Text> */}
+      <S.Text>Feito com amor e carinho para ganhar o shape da FIAP</S.Text>
+      <S.SocialMedia>
+        <img src={insta} alt="insta" sizes={50} />
+        <img src={face} alt="insta" sizes={50} />
+        <img src={linkedin} alt="insta" sizes={50} />
+      </S.SocialMedia>
+      {/* </S.MediaWrapper> */}
+    </S.Container>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,14 +1,36 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.footer`
+  position: absolute;
+  width: 100%;
+  height: 150px;
+  left: 0px;
+  margin-top: 40px;
+  display: flex;
+  align-items: center;
+  /* justify-content: flex-end; */
+  justify-content: space-between;
 
-    position: absolute;
-    width: 100%;
-    height: 300px;
-    left: 0px;
-    margin-top: 40px;
+  /* Goiaba */
 
-/* Goiaba */
+  background: #e2645a;
+`;
+export const MediaWrapper = styled.div`
+  justify-content: flex-end;
 
-background: #E2645A;
-`
+  padding-top: 70px;
+`;
+
+export const SocialMedia = styled.div`
+  width: 20%;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin-right: 50px;
+`;
+
+export const Text = styled.span`
+  color: #fff;
+  margin-left: 50px;
+  font-size: 20px;
+`;
